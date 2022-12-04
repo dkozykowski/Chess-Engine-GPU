e42ef18a92396a191830485610c1ee6601fb5ae0
Jan Karchut <jkarchut00@gmail.com>
2022-12-04 12:22:58 +0100

    add knight, and rook moves generation tests

--- test/tests.cu
+++ test/tests.cu
@@ -223,7 +223,233 @@ TEST(SearchForBestMovesTest, StartPositionBestMoveSearchForBlack) {
 }
 
 
+TEST(GenerateMovesTest, WhiteKnightMovesTests) {
+    // given
+    pos64 white_pawns = WHITE_PAWN_STARTING_POS;
+    pos64 white_bishops = WHITE_BISHOP_STARTING_POS;
+    pos64 white_knights = WHITE_KNIGHT_STARTING_POS;
+    pos64 white_rooks = WHITE_ROOK_STARTING_POS;
+    pos64 white_queens = WHITE_QUEEN_STARTING_POS;
+    pos64 white_kings = WHITE_KING_STARTING_POS;
+
+    pos64 black_pawns = 0;
+    pos64 black_bishops = 0;
+    pos64 black_knights = 536870912;
+    pos64 black_rooks = 0;
+    pos64 black_queens = 0;
+    pos64 black_kings = 0;
+
+    short current_player = 0;
+
+    pos64 *white_pawn_moves, *white_bishop_moves, *white_knight_moves, *white_rook_moves, *white_queen_moves, *white_king_moves, 
+        *black_pawn_moves, *black_bishop_moves, *black_knight_moves, *black_rook_moves, *black_queen_moves, *black_king_moves;
+
+    white_pawn_moves = new pos64[BOARDS_GENERATED];
+    white_bishop_moves = new pos64[BOARDS_GENERATED];
+    white_knight_moves = new pos64[BOARDS_GENERATED];
+    white_rook_moves = new pos64[BOARDS_GENERATED];
+    white_queen_moves = new pos64[BOARDS_GENERATED];
+    white_king_moves = new pos64[BOARDS_GENERATED];
+    black_pawn_moves = new pos64[BOARDS_GENERATED];
+    black_bishop_moves = new pos64[BOARDS_GENERATED];
+    black_knight_moves = new pos64[BOARDS_GENERATED];
+    black_rook_moves = new pos64[BOARDS_GENERATED];
+    black_queen_moves = new pos64[BOARDS_GENERATED];
+    black_king_moves = new pos64[BOARDS_GENERATED];
+
+    // when
+    generate_moves(&white_pawns, &white_bishops, &white_knights, &white_rooks, &white_queens, &white_kings, 
+                &black_pawns, &black_bishops, &black_knights, &black_rooks, &black_queens, &black_kings,
+                white_pawn_moves, white_bishop_moves, white_knight_moves, white_rook_moves, white_queen_moves, white_king_moves, 
+                black_pawn_moves, black_bishop_moves, black_knight_moves, black_rook_moves, black_queen_moves,  black_king_moves,
+                current_player);
+    int generated_moves_count = 0;
+    int generated_attacks_count = 0;
+    for(int x = 0; x < BOARDS_GENERATED; x++)
+    {
+        if (white_king_moves[x]) == 0) break;
+        if(white_pawn_moves[x] < white_pawns){ 
+            generated_attacks_count++;
+        }
+        generated_moves_count = x + 1;
+    }
+
+    // then 
+    ASSERT_EQ(8, generated_moves_count);
+    ASSERT_EQ(2, generated_attacks_count);
+}
+
+TEST(GenerateMovesTest, BlackKnightMovesTests) {
+    // given
+    pos64 white_pawns = 0;
+    pos64 white_bishops = 0;
+    pos64 white_knights = 268435456;
+    pos64 white_rooks = 0;
+    pos64 white_queens = 0;
+    pos64 white_kings = 0;
+
+    pos64 black_pawns = BLACK_PAWN_STARTING_POS;
+    pos64 black_bishops = BLACK_BISHOP_STARTING_POS;
+    pos64 black_knights = BLACK_KNIGHT_STARTING_POS;
+    pos64 black_rooks = BLACK_ROOK_STARTING_POS;
+    pos64 black_queens = BLACK_QUEEN_STARTING_POS;
+    pos64 black_kings = BLACK_KING_STARTING_POS;
+
+    short current_player = 1;
+
+    pos64 *white_pawn_moves, *white_bishop_moves, *white_knight_moves, *white_rook_moves, *white_queen_moves, *white_king_moves, 
+        *black_pawn_moves, *black_bishop_moves, *black_knight_moves, *black_rook_moves, *black_queen_moves, *black_king_moves;
+
+    white_pawn_moves = new pos64[BOARDS_GENERATED];
+    white_bishop_moves = new pos64[BOARDS_GENERATED];
+    white_knight_moves = new pos64[BOARDS_GENERATED];
+    white_rook_moves = new pos64[BOARDS_GENERATED];
+    white_queen_moves = new pos64[BOARDS_GENERATED];
+    white_king_moves = new pos64[BOARDS_GENERATED];
+    black_pawn_moves = new pos64[BOARDS_GENERATED];
+    black_bishop_moves = new pos64[BOARDS_GENERATED];
+    black_knight_moves = new pos64[BOARDS_GENERATED];
+    black_rook_moves = new pos64[BOARDS_GENERATED];
+    black_queen_moves = new pos64[BOARDS_GENERATED];
+    black_king_moves = new pos64[BOARDS_GENERATED];
+
+    // when
+    generate_moves(&white_pawns, &white_bishops, &white_knights, &white_rooks, &white_queens, &white_kings, 
+                &black_pawns, &black_bishops, &black_knights, &black_rooks, &black_queens, &black_kings,
+                white_pawn_moves, white_bishop_moves, white_knight_moves, white_rook_moves, white_queen_moves, white_king_moves, 
+                black_pawn_moves, black_bishop_moves, black_knight_moves, black_rook_moves, black_queen_moves,  black_king_moves,
+                current_player);
+        int generated_moves_count = 0;
+    int generated_attacks_count = 0;
+    for(int x = 0; x < BOARDS_GENERATED; x++)
+    {
+        if (black_king_moves[x]) == 0) break;
+        if(black_pawn_moves[x] < black_pawns){ 
+            generated_attacks_count++;
+        }
+        generated_moves_count = x + 1;
+    }
+
+    // then 
+    ASSERT_EQ(8, generated_moves_count);
+    ASSERT_EQ(2, generated_attacks_count);
+}
+
+TEST(GenerateMovesTest, WhiteRookMovesTests) {
+    // given
+    pos64 white_pawns = WHITE_PAWN_STARTING_POS;
+    pos64 white_bishops = WHITE_BISHOP_STARTING_POS;
+    pos64 white_knights = WHITE_KNIGHT_STARTING_POS;
+    pos64 white_rooks = WHITE_ROOK_STARTING_POS;
+    pos64 white_queens = WHITE_QUEEN_STARTING_POS;
+    pos64 white_kings = WHITE_KING_STARTING_POS;
+
+    pos64 black_pawns = 0;
+    pos64 black_bishops = 0;
+    pos64 black_knights = 0;
+    pos64 black_rooks = 536870912;
+    pos64 black_queens = 0;
+    pos64 black_kings = 0;
+
+    short current_player = 0;
+
+    pos64 *white_pawn_moves, *white_bishop_moves, *white_knight_moves, *white_rook_moves, *white_queen_moves, *white_king_moves, 
+        *black_pawn_moves, *black_bishop_moves, *black_knight_moves, *black_rook_moves, *black_queen_moves, *black_king_moves;
+
+    white_pawn_moves = new pos64[BOARDS_GENERATED];
+    white_bishop_moves = new pos64[BOARDS_GENERATED];
+    white_knight_moves = new pos64[BOARDS_GENERATED];
+    white_rook_moves = new pos64[BOARDS_GENERATED];
+    white_queen_moves = new pos64[BOARDS_GENERATED];
+    white_king_moves = new pos64[BOARDS_GENERATED];
+    black_pawn_moves = new pos64[BOARDS_GENERATED];
+    black_bishop_moves = new pos64[BOARDS_GENERATED];
+    black_knight_moves = new pos64[BOARDS_GENERATED];
+    black_rook_moves = new pos64[BOARDS_GENERATED];
+    black_queen_moves = new pos64[BOARDS_GENERATED];
+    black_king_moves = new pos64[BOARDS_GENERATED];
+
+    // when
+    generate_moves(&white_pawns, &white_bishops, &white_knights, &white_rooks, &white_queens, &white_kings, 
+                &black_pawns, &black_bishops, &black_knights, &black_rooks, &black_queens, &black_kings,
+                white_pawn_moves, white_bishop_moves, white_knight_moves, white_rook_moves, white_queen_moves, white_king_moves, 
+                black_pawn_moves, black_bishop_moves, black_knight_moves, black_rook_moves, black_queen_moves,  black_king_moves,
+                current_player);
+    int generated_moves_count = 0;
+    int generated_attacks_count = 0;
+    for(int x = 0; x < BOARDS_GENERATED; x++)
+    {
+        if (white_king_moves[x]) == 0) break;
+        if(white_pawn_moves[x] < white_pawns){ 
+            generated_attacks_count++;
+        }
+        generated_moves_count = x + 1;
+    }
+
+    // then 
+    ASSERT_EQ(13, generated_moves_count);
+    ASSERT_EQ(1, generated_attacks_count);
+}
+
+TEST(GenerateMovesTest, BlackRookMovesTests) {
+    // given
+    pos64 white_pawns = 0;
+    pos64 white_bishops = 0;
+    pos64 white_knights = 0;
+    pos64 white_rooks = 268435456;
+    pos64 white_queens = 0;
+    pos64 white_kings = 0;
+
+    pos64 black_pawns = BLACK_PAWN_STARTING_POS;
+    pos64 black_bishops = BLACK_BISHOP_STARTING_POS;
+    pos64 black_knights = BLACK_KNIGHT_STARTING_POS;
+    pos64 black_rooks = BLACK_ROOK_STARTING_POS;
+    pos64 black_queens = BLACK_QUEEN_STARTING_POS;
+    pos64 black_kings = BLACK_KING_STARTING_POS;
+
+    short current_player = 1;
+
+    pos64 *white_pawn_moves, *white_bishop_moves, *white_knight_moves, *white_rook_moves, *white_queen_moves, *white_king_moves, 
+        *black_pawn_moves, *black_bishop_moves, *black_knight_moves, *black_rook_moves, *black_queen_moves, *black_king_moves;
+
+    white_pawn_moves = new pos64[BOARDS_GENERATED];
+    white_bishop_moves = new pos64[BOARDS_GENERATED];
+    white_knight_moves = new pos64[BOARDS_GENERATED];
+    white_rook_moves = new pos64[BOARDS_GENERATED];
+    white_queen_moves = new pos64[BOARDS_GENERATED];
+    white_king_moves = new pos64[BOARDS_GENERATED];
+    black_pawn_moves = new pos64[BOARDS_GENERATED];
+    black_bishop_moves = new pos64[BOARDS_GENERATED];
+    black_knight_moves = new pos64[BOARDS_GENERATED];
+    black_rook_moves = new pos64[BOARDS_GENERATED];
+    black_queen_moves = new pos64[BOARDS_GENERATED];
+    black_king_moves = new pos64[BOARDS_GENERATED];
+
+    // when
+    generate_moves(&white_pawns, &white_bishops, &white_knights, &white_rooks, &white_queens, &white_kings, 
+                &black_pawns, &black_bishops, &black_knights, &black_rooks, &black_queens, &black_kings,
+                white_pawn_moves, white_bishop_moves, white_knight_moves, white_rook_moves, white_queen_moves, white_king_moves, 
+                black_pawn_moves, black_bishop_moves, black_knight_moves, black_rook_moves, black_queen_moves,  black_king_moves,
+                current_player);
+        int generated_moves_count = 0;
+    int generated_attacks_count = 0;
+    for(int x = 0; x < BOARDS_GENERATED; x++)
+    {
+        if (black_king_moves[x]) == 0) break;
+        if(black_pawn_moves[x] < black_pawns){ 
+            generated_attacks_count++;
+        }
+        generated_moves_count = x + 1;
+    }
+
+    // then 
+    ASSERT_EQ(13, generated_moves_count);
+    ASSERT_EQ(1, generated_attacks_count);
+}
+
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+
