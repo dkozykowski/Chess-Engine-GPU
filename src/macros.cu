40dacbdd8775b64e262a2a58609c84f741ac03b0
dkozykowski <dkozykowski@gmail.com>
2022-12-27 16:44:37 +0100

    remove inline keywoard

--- src/macros.cu
+++ src/macros.cu
@@ -1,6 +1,6 @@
 #include "macros.cuh"
 
-inline void gpuAssert(cudaError_t code, const char *file, int line) {
+void gpuAssert(cudaError_t code, const char *file, int line) {
     if (code != cudaSuccess) {
         fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file,
                 line);
