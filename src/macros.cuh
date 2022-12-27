3350cee4654de6b2a8cb0b335f7c3690d8c7471a
dkozykowski <dkozykowski@gmail.com>
2022-12-27 16:29:45 +0100

    move gpu assert to macros

--- src/macros.cuh
+++ src/macros.cuh
@@ -21,6 +21,20 @@ typedef unsigned long long int pos64;
     (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), \
      exit(EXIT_FAILURE))
 
+#define gpuErrchk(ans) \
+    { gpuAssert((ans), __FILE__, __LINE__); }
+
+inline void gpuAssert(cudaError_t code, const char *file, int line,
+                      bool abort = true) {
+    if (code != cudaSuccess) {
+        fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file,
+                line);
+        if (abort) {
+            exit(code);
+        }
+    }
+}
+
 #define MAX_THREADS 256
 #define MAX_BLOCKS_PER_DIMENSION 65535
 
