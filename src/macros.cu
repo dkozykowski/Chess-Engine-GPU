7d6e6391c33c63f22530de8a1d5c703eda08eb70
dkozykowski <dkozykowski@gmail.com>
2022-12-27 16:32:48 +0100

    add missing file

---
+++ src/macros.cu
@@ -0,0 +1,10 @@
+inline void gpuAssert(cudaError_t code, const char *file, int line,
+                      bool abort = true) {
+    if (code != cudaSuccess) {
+        fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file,
+                line);
+        if (abort) {
+            exit(code);
+        }
+    }
+}
